ddce23068c991e50a7ef88d9a5df7ebc77760c80
obimbasmart <obimbasmartchukwunenye@gmail.com>
2024-03-28 19:42:03 +0100

    fix: use mongodb command

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -12,16 +12,17 @@ Write a Python function that returns all students sorted by average score:
 
 def top_students(mongo_collection):
     """get top students"""
-    students = [student for student in mongo_collection.find()]
-    for student in students:
-        average_score = sum([topic["score"]
-                             for topic in
-                             student["topics"]]) / 3
+    return mongo_collection.aggregate(
+        [
+            {
+                "$project": {
+                    "name": "$name",
+                    "averageScore": {"$avg": "$topics.score"}
+                }
+            },
 
-        mongo_collection.update_one({"_id": student["_id"]},
-                                    {
-                                        "$set": {"averageScore": average_score}
-        })
-
-    return sorted([student for student in mongo_collection.find()],
-                  key=lambda x: x['averageScore'], reverse=True)
+            {
+                "$sort": {"averageScore": -1}
+            }
+        ]
+    )
