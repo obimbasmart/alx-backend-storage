1ac1e30b1541bc25cd73be1b9e226177ae857f5f
obimbasmart <obimbasmartchukwunenye@gmail.com>
2024-03-28 14:47:55 +0100

    fix: incorrect output

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -16,21 +16,19 @@ def nginx_stats(db, collection):
     client = MongoClient()
     server_logs_collection = client[db][collection]
     logs = [log for log in server_logs_collection.find({})]
-
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
 
     grouped_logs = {
         method: len([log for log in logs if log['method'] == method])
         for method in methods
     }
-
     status_check = len([log for log in logs
                         if log['method'] == "GET" and
                         log['path'] == '/status'
                         ])
 
-    return ('{} logs\nMethods:\n\tmethod GET {}\n\tmethod POST {}\n\tmethod '
-            'PUT {}\n\tmethod PATCH {}\n\tmethod DELETE {}\n{} status check'
+    return ('{} logs\nMethods:\n    method GET: {}\n    method POST: {}\n    method '
+            'PUT: {}\n    method PATCH: {}\n    method DELETE: {}\n{} status check'
             .format(
                 len(logs),
                 *grouped_logs.values(), status_check
