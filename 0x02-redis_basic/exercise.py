f06cd63f09bae899755f7ace37849974eda7796d
obimbasmart <obimbasmartchukwunenye@gmail.com>
2024-03-29 22:02:00 +0100

    fix: use private attr

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -13,11 +13,11 @@ class Cache:
     def __init__(self) -> None:
         """init redis cache"""
 
-        self.__redis = redis.Redis()
-        self.__redis.flushdb()
+        self._redis = redis.Redis()
+        self._redis.flushdb()
 
     def store(self, data: Union[int, float, bytes, str]) -> str:
         """store the input data in Redis"""
         key = str(uuid.uuid1())
-        self.__redis.set(key, data)
+        self._redis.set(key, data)
         return key
