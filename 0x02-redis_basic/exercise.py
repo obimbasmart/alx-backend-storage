301ef5c5d08a049609b205ad2d9ad1388b4020da
obimbasmart <obimbasmartchukwunenye@gmail.com>
2024-03-29 21:56:42 +0100

    init: implement 1st task - redis cache

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,23 @@
+# #!/usr/bin/env python3
+
+"""Redis cache implementtion"""
+
+import redis
+import uuid
+from typing import Union
+
+
+class Cache:
+    """cache class"""
+
+    def __init__(self) -> None:
+        """init redis cache"""
+
+        self.__redis = redis.Redis()
+        self.__redis.flushdb()
+
+    def store(self, data: Union[int, float, bytes, str]) -> str:
+        """store the input data in Redis"""
+        key = str(uuid.uuid1())
+        self.__redis.set(key, data)
+        return key
