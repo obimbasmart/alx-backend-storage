4e6f462b9906e589f5456072cfb0636626acea8d
obimbasmart <obimbasmartchukwunenye@gmail.com>
2024-03-30 21:58:19 +0100

    add: track ntimes function calls

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -11,8 +11,10 @@ r = redis.Redis()
 
 def get_page(url: str) -> str:
     """obtain html content of a web page
+        track ntimes func is called
         and cache result for 10s
     """
+    r.incr(f'count:{url}')
     cached_result = r.get(url)
     if cached_result:
         return cached_result
